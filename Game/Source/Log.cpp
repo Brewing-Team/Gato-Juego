11b300cc9553e7bac18006b89999a6eb5217fb7d
Hugo Planell Moreno <hugoplanellmoreno@gmail.com>
2023-10-17 11:36:33 +0200

    Log full-path / relative-path options

--- Game/Source/Log.cpp
+++ Game/Source/Log.cpp
@@ -5,15 +5,37 @@
 #include <stdio.h>
 #include <cstdarg>
 
+// #ifdef FULL_PATH
+// void Log(const char* file, int line, const char* format, ...) {
+// 	va_list args;
+// 	va_start(args, format);
+
+// 	printf("[%s:%d] ", file, line);
+// 	vprintf(format, args);
+// 	printf("\n");
+
+// 	va_end(args);
+// }
+//#elif
+#include <string>
 void Log(const char* file, int line, const char* format, ...) {
-    va_list args;
-    va_start(args, format);
+	va_list args;
+	va_start(args, format);
 
-    printf("[%s:%d] ", file, line);
-    vprintf(format, args);
+	std::string filePath(file);
+	size_t lastSlash = filePath.find_last_of("/\\");
+	if (lastSlash != std::string::npos) {
+		filePath = filePath.substr(lastSlash + 1);
+	}
 
-    va_end(args);
+	printf("[%s:%d] ", filePath.c_str(), line);
+	vprintf(format, args);
+	printf("\n");
+
+	va_end(args);
 }
+//#endif
+
 
 #elif _WIN32
 
--- Game/Source/Log.h
+++ Game/Source/Log.h
@@ -1,6 +1,9 @@
 #ifndef __LOG_H__
 #define __LOG_H__
 
+//With this you can choose between full-path printing or relative-path printing
+#define FULL_PATH 1
+
 #define LOG(format, ...) Log(__FILE__, __LINE__, format, ##__VA_ARGS__)
 
 void Log(const char file[], int line, const char* format, ...);
