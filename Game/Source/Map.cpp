2585f8aacae6caefe0e79e2e3e762273a8e9f926
Hugo Planell <hugoplanellmoreno@gmail.com>
2023-11-22 13:28:58 +0100

    camera clipping working with scale 1

--- Game/Source/Map.cpp
+++ Game/Source/Map.cpp
@@ -61,15 +61,15 @@ bool Map::Update(float dt)
 
         if (mapLayerItem->data->properties.GetProperty("Draw") != NULL && mapLayerItem->data->properties.GetProperty("Draw")->value) {
 
-            iPoint cameraPosMap = WorldToMap((int)(app->render->camera.x / (float)app->win->GetScale()), (int)(app->render->camera.y / (float)app->win->GetScale()));
-
+            iPoint cameraPosMap = WorldToMap((int)(-app->render->camera.x), (int)(-app->render->camera.y));
+            //cameraPosMap = { 80,80 };
 
 
             int leftClipping = MAX(cameraPosMap.x, 0);
-            int rightClipping = MIN(cameraPosMap.x + (int)((app->render->camera.w / (float)app->win->GetScale()) / mapData.tileWidth), mapLayerItem->data->width);
+            int rightClipping = MIN((cameraPosMap.x + (int)(app->render->camera.w / mapData.tileWidth)), mapLayerItem->data->width);
             
-            int topClipping = MAX(-cameraPosMap.y, 0);
-            int bottomClipping = MIN(-cameraPosMap.y + (int)((app->render->camera.h / (float)app->win->GetScale()) / mapData.tileHeight), mapLayerItem->data->height);
+            int topClipping = MAX(cameraPosMap.y, 0);
+            int bottomClipping = MIN((cameraPosMap.y + (int)(app->render->camera.h / mapData.tileHeight)), mapLayerItem->data->height);
 
             LOG("cameraPosMap: (%d, %d)", cameraPosMap.x, cameraPosMap.y);
             LOG("%d, %d, %d, %d", leftClipping, rightClipping, topClipping, bottomClipping);
@@ -90,7 +90,7 @@ bool Map::Update(float dt)
                         pos.x,
                         pos.y,
                         &r,
-                        mapLayerItem->data->parallaxFactor);
+                        1.0f);
                 }
             }
         }
